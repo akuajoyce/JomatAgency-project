9c3ba494afc1a01080d495421919f548c0773c47
Your Name <ashawaabbey@gmail.com>
2025-04-15 15:38:56 +0000

    created bookform

---
+++ src/pages/parent-student/BookForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const BookForm = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1>Online Booking Teacher Form</h1>
+            </div>
+
+            <form>
+            <div className="mt-4">
+            <label className="text-gray-700 font-medium">
+              Name <span className="text-red-500">*</span>
+            </label>
+          </div>
+
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default BookForm
